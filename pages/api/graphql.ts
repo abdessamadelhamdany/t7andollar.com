17e74ca5156e351a4f1a55a7987c679753330fc2
Abdessamad El Hamdany <abdessamadelhamdany@gmail.com>
2021-12-06 14:15:09 +0100

    install and setup apollo server

---
+++ pages/api/graphql.ts
@@ -0,0 +1,47 @@
+import { gql, ApolloServer } from 'apollo-server-micro';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+const typeDefs = gql`
+  type User {
+    id: ID
+  }
+
+  type Query {
+    getUser: User
+  }
+`;
+
+const resolvers = {
+  Query: {
+    getUser: () => {
+      return {
+        id: 'Foo',
+      };
+    },
+  },
+};
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+});
+
+const startServer = apolloServer.start();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  await startServer;
+  await apolloServer.createHandler({
+    path: '/api/graphql',
+  })(req, res);
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
