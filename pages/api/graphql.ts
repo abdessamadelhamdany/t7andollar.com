43d657bbb57136567f5d6f10c097aac5555898f2
Abdessamad El Hamdany <abdessamadelhamdany@gmail.com>
2021-12-07 11:15:43 +0100

    cleanup micro cors

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,19 +1,18 @@
-import Cors from 'micro-cors'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { ApolloServer } from 'apollo-server-micro'
 import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core'
 import { typeDefs } from '../../graphql/schema'
 import { resolvers } from '../../graphql/resolvers'
 
-const cors = Cors()
 const apolloServer = new ApolloServer({
   typeDefs,
   resolvers,
   plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
 })
+
 const startServer = apolloServer.start()
 
-export default cors(async function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
@@ -21,7 +20,7 @@ export default cors(async function handler(
   await apolloServer.createHandler({
     path: '/api/graphql',
   })(req, res)
-} as any)
+}
 
 export const config = {
   api: {
