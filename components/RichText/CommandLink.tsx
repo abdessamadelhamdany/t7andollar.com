f87c066a3b852b87fff94e517bc86f9977cc786c
Abdessamad El Hamdany <abdessamadelhamdany@gmail.com>
2021-12-09 23:06:41 +0100

    Show/Hide CommandLinkPopup

--- components/RichText/CommandLink.tsx
+++ components/RichText/CommandLink.tsx
@@ -1,18 +1,40 @@
-import React, { MouseEvent } from 'react';
+import React, { MouseEvent, useEffect, useState } from 'react';
 import Input from '../Input';
 import CommandButton from './CommandButton';
 import classes from './CommandLink.module.scss';
 
 const CommandLink = () => {
+  const [commandLinkPopupOpen, setCommandLinkPopupOpen] = useState(false);
+
+  const closeCommandLinkPopup = () => {
+    setCommandLinkPopupOpen(false);
+  };
+  const openCommandLinkPopup = () => {
+    setCommandLinkPopupOpen(true);
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', closeCommandLinkPopup);
+    return () => {
+      document.removeEventListener('click', closeCommandLinkPopup);
+    };
+  }, []);
+
   const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
+    openCommandLinkPopup();
   };
 
   return (
-    <div className={classes.commandLink}>
+    <div className={classes.commandLink} onClick={(e) => e.stopPropagation()}>
       <CommandButton icon="link" onClick={handleClick} />
 
-      <div className={classes.commandLinkPopup}>
+      <div
+        className={[
+          classes.commandLinkPopup,
+          commandLinkPopupOpen ? classes.commandLinkPopupOpen : '',
+        ].join(' ')}
+      >
         <Input
           className={classes.commandLinkInput}
           type="text"
