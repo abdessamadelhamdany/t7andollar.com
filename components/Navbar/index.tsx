73160da5abcad87f52a68c08345f2a8856c65b84
Abdessamad El Hamdany <abdessamadelhamdany@gmail.com>
2021-12-18 21:14:31 +0100

    add dynamic active state to navbar

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -2,9 +2,21 @@ import Link from 'next/link';
 import React, { FC } from 'react';
 import { useUser } from 'store/hooks';
 import LogoutButton from '@/components/LogoutButton';
+import { useRouter } from 'next/router';
 
 const Navbar: FC = () => {
   const { authUser } = useUser();
+  const router = useRouter();
+
+  const activeLinkClasses = (href: string) => {
+    let classes = ['nav-link'];
+
+    if (router.asPath === href) {
+      classes.push('active');
+    }
+
+    return classes.join(' ');
+  };
 
   return (
     <nav className="topnav navbar navbar-expand-lg navbar-light bg-white fixed-top border-bottom border-gray">
@@ -29,28 +41,41 @@ const Navbar: FC = () => {
         <div className="navbar-collapse collapse" id="main-navbar">
           <ul className="navbar-nav mr-auto d-flex align-items-center">
             <li className="nav-item">
-              <Link href="/category/freelancing">
-                <a className="nav-link active">العمل على الانترنت</a>
+              <Link href="/category/جني-المال-من-الإنترنت">
+                <a
+                  className={activeLinkClasses(
+                    '/category/جني-المال-من-الإنترنت'
+                  )}
+                >
+                  جني المال من الإنترنت
+                </a>
               </Link>
             </li>
             <li className="nav-item">
-              <Link href="/category/make-money-online">
-                <a className="nav-link">ربح المال من الانترنت</a>
+              <Link href="/category/تحسين-محركات-البحث">
+                <a
+                  className={activeLinkClasses('/category/تحسين-محركات-البحث')}
+                >
+                  تحسين محركات البحث
+                </a>
               </Link>
             </li>
             <li className="nav-item">
-              <Link href="/category/search-engine-optimization">
-                <a className="nav-link">تحسين محركات البحث</a>
+              <Link href="/category/انشاء-المواقع-الالكترونية">
+                <a
+                  className={activeLinkClasses(
+                    '/category/انشاء-المواقع-الالكترونية'
+                  )}
+                >
+                  انشاء المواقع الالكترونية
+                </a>
               </Link>
             </li>
             <li className="nav-item">
-              <Link href="/category/how-to-make-websites">
-                <a className="nav-link">انشاء المواقع الالكترونية</a>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link href="/category/latest-tech-news">
-                <a className="nav-link">أخبار تقنية</a>
+              <Link href="/category/أخبار-تقنية">
+                <a className={activeLinkClasses('/category/أخبار-تقنية')}>
+                  أخبار تقنية
+                </a>
               </Link>
             </li>
             {authUser && (
